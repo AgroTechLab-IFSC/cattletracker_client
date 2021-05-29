a00ea420574777df381d3447893a6e4fd18dd4eb
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-05-29 20:49:20 -0300

    Mostrando dashborad de forma diferente para mobile e web

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,6 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html';
-// import 'dart:io' if (dart.library.html) 'dart:ui' as ui;
-import 'dart:ui' as ui;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../utils/dashboard.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,39 +11,14 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.blueAccent,
-          title: Text("Cattletracker Client Dashboard"),
-          actions: <Widget>[
-            Row(
-              mainAxisSize: MainAxisSize.min,
-            )
-          ]),
-      body: _uiSetup(),
-    );
-  }
-
-  Widget _uiSetup() {
-    // ignore: undefined_prefixed_name
-    ui.platformViewRegistry.registerViewFactory(
-        'hello-html',
-        (int viewId) => IFrameElement()
-          ..width = '640'
-          ..height = '360'
-          ..src =
-              'https://agrotechlab.lages.ifsc.edu.br:8080/dashboard/06ba1eb0-7d3d-11eb-8968-31bbd555ab9f'
-          ..style.border = 'none');
-
-    return new Container(
-      alignment: Alignment.center,
-      child: Padding(
-        padding: EdgeInsets.all(25),
-        child: FractionallySizedBox(
-          widthFactor: 1,
-          heightFactor: 1,
-          child: HtmlElementView(viewType: 'hello-html'),
-        ),
-      ),
-    );
+        appBar: AppBar(
+            backgroundColor: Colors.blueAccent,
+            title: Text("Cattletracker Client Dashboard"),
+            actions: <Widget>[
+              Row(
+                mainAxisSize: MainAxisSize.min,
+              )
+            ]),
+        body: Dashboard().returnDashboard());
   }
 }
